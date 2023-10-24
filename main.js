6cbc2770c7aacbbcbab1c5a6eae8cc799386491b
Djeddi-Blue <slim.narrow.fast@gmail.com>
2023-10-24 20:02:49 +0200

    ajout de l'index html

---
+++ main.js
@@ -0,0 +1,59 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
+
+
+
+ const section = document.querySelectorAll('section');
+ var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  // ################
+  // slider prochainement
+  // ################
+  
+  var swiper = new Swiper('.coming-container', {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 55000,
+      disabledOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      568: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      968: {
+        slidesPerView: 5,
+      }
+    }
+  })
+
